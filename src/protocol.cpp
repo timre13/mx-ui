9dd2ffea1778c9e1cbf37d50a8be2514ff4c950c
timre13 <itorteli13@gmail.com>
2024-10-28 23:42:40 +0100

    Refactor serial communication

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -4,7 +4,6 @@
 #include <fcntl.h>
 #include <termios.h>
 #else
-#error Only Linux is supported
 #endif
 #include <iostream>
 #include <stdint.h>
@@ -46,6 +45,113 @@ std::string connStatusGetColor(ConnStatus cs)
     assert(false);
 }
 
+// Platform-specific state
+struct PlatformState
+{
+#ifdef __linux__
+    int port{};
+    termios oldTio{};
+#else
+
+#endif
+};
+
+#ifdef __linux__
+
+static int configurePort(PlatformState* state)
+{
+    state->port = open("/dev/ttyUSB0", O_RDONLY | O_NOCTTY);
+
+    if (state->port == -1)
+    {
+        std::cerr << "Failed to open port: " << strerror(errno) << '\n';
+        return 1;
+    }
+
+    std::cout << "Opened port, handle: " << state->port << '\n';
+
+    state->oldTio = termios{};
+    tcgetattr(state->port, &state->oldTio);
+
+    tcflush(state->port, TCIFLUSH);
+
+    termios newTio = termios{};
+    newTio.c_cflag = CRTSCTS | CS8 | CLOCAL | CREAD;
+    newTio.c_iflag = IGNPAR;
+    newTio.c_oflag = 0;
+    newTio.c_lflag = 0;
+    newTio.c_cc[VTIME] = 0;
+    newTio.c_cc[VMIN] = READ_MIN;
+    cfsetispeed(&newTio, B2400);
+    tcsetattr(state->port, TCSANOW, &newTio);
+
+    return 0;
+}
+
+#else
+
+static int configurePort(PlatformState* state)
+{
+    std::cout << "TODO\n";
+    (void)state;
+    return 1;
+}
+
+#endif
+
+#ifdef __linux__
+
+static int readFrame(ConnStatus* connStatus, PlatformState* state, uint8_t* buf)
+{
+    fd_set fdSet;
+    FD_ZERO(&fdSet);
+    FD_SET(state->port, &fdSet);
+    timeval timeout{.tv_usec = READ_TIMEOUT_USEC};
+    int retVal = select(state->port+1, &fdSet, nullptr, nullptr, &timeout);
+    if (retVal == -1)
+    {
+        std::cerr << "select() error: " << strerror(errno) << '\n';
+        *connStatus = ConnStatus::IOError;
+        return 1;
+    }
+    if (retVal == 0)
+    {
+        std::cerr << "Timed out" << '\n';
+        *connStatus = ConnStatus::Timeout;
+        return 1;
+    }
+
+    int count = read(state->port, buf, 255);
+    if (count == -1)
+    {
+        std::cerr << "I/O error: " << strerror(errno) << '\n';
+        *connStatus = ConnStatus::IOError;
+        return 1;
+    }
+    if (count == 0)
+    {
+        std::cout << "EOF\n";
+        *connStatus = ConnStatus::Eof;
+        tcsetattr(state->port, TCSANOW, &state->oldTio);
+        close(state->port);
+        return 1;
+    }
+    return 0;
+}
+
+#else
+
+static int readFrame(ConnStatus* connStatus, PlatformState* state, uint8_t* buf)
+{
+    std::cout << "TODO\n";
+    *connStatus = ConnStatus::Closed;
+    (void)state;
+    (void)buf;
+    return 1;
+}
+
+#endif
+
 void startReadingData(
         const std::atomic<bool>& keepThreadAlive, std::atomic<bool>& stayConnected, ConnStatus& connStatus,
         std::vector<std::unique_ptr<Frame>>& frames, std::mutex& framesMutex,
@@ -69,33 +175,14 @@ void startReadingData(
         connStatus = ConnStatus::Connecting;
         dispatcher.emit();
 
-        const int port = open("/dev/ttyUSB0", O_RDONLY | O_NOCTTY);
-
-        if (port == -1)
+        PlatformState state;
+        if (configurePort(&state))
         {
-            std::cerr << "Failed to open port: " << strerror(errno) << '\n';
-            connStatus = ConnStatus::FailedToOpen;
-            dispatcher.emit();
-            stayConnected = false;
-            continue;
+           connStatus = ConnStatus::FailedToOpen;
+           dispatcher.emit();
+           stayConnected = false;
+           continue;
         }
-
-        std::cout << "Opened port, handle: " << port << '\n';
-
-        termios oldTio = termios{};
-        tcgetattr(port, &oldTio);
-
-        tcflush(port, TCIFLUSH);
-
-        termios newTio = termios{};
-        newTio.c_cflag = CRTSCTS | CS8 | CLOCAL | CREAD;
-        newTio.c_iflag = IGNPAR;
-        newTio.c_oflag = 0;
-        newTio.c_lflag = 0;
-        newTio.c_cc[VTIME] = 0;
-        newTio.c_cc[VMIN] = READ_MIN;
-        cfsetispeed(&newTio, B2400);
-        tcsetattr(port, TCSANOW, &newTio);
 
         uint8_t buf[255]{};
 
@@ -113,45 +200,10 @@ void startReadingData(
                 break;
             }
 
-            fd_set fdSet;
-            FD_ZERO(&fdSet);
-            FD_SET(port, &fdSet);
-            timeval timeout{.tv_usec = READ_TIMEOUT_USEC};
-            int retVal = select(port+1, &fdSet, nullptr, nullptr, &timeout);
-            if (retVal == -1)
+            if (readFrame(&connStatus, &state, buf))
             {
-                std::cerr << "select() error: " << strerror(errno) << '\n';
-                connStatus = ConnStatus::IOError;
                 dispatcher.emit();
                 stayConnected = false;
-                break;
-            }
-            if (retVal == 0)
-            {
-                std::cerr << "Timed out" << '\n';
-                connStatus = ConnStatus::Timeout;
-                dispatcher.emit();
-                stayConnected = false;
-                break;
-            }
-
-            int count = read(port, buf, 255);
-            if (count == -1)
-            {
-                std::cerr << "I/O error: " << strerror(errno) << '\n';
-                connStatus = ConnStatus::IOError;
-                dispatcher.emit();
-                stayConnected = false;
-                break;
-            }
-            if (count == 0)
-            {
-                std::cout << "EOF\n";
-                connStatus = ConnStatus::Eof;
-                dispatcher.emit();
-                stayConnected = false;
-                tcsetattr(port, TCSANOW, &oldTio);
-                close(port);
                 break;
             }
 
